ba73606368c8652d2c10fc86bbf09c2b8222ef9c
Kashaf Yaseen <yaseenkashaf9@gmail.com>
2022-08-10 17:55:01 +0500

    Username added to authentication

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,11 @@
 class Stock < ApplicationRecord
 
+
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
+
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                       endpoint: 'https://sandbox.iexapis.com/v1')
@@ -8,7 +14,12 @@ class Stock < ApplicationRecord
         rescue => exception
             return nil
         end 
-        #client.price(ticker_symbol)
     end 
+
+    def self.check_db(ticker_symbol)
+        where(ticker: ticker_symbol).first
+    end 
+
+
     
 end
